970dfa0c3e9192a8686252e0db96f6842383fbb7
Michael L <lopezma6@uci.edu>
2026-01-11 13:45:52 -0800

    Second Actual Commit
    I'm pretty sure I've finished creating the game, hopefully.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,48 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        // 1. Get input from the keyboard
+        // "Horizontal" handles A, D, Left Arrow, Right Arrow
+        // "Vertical" handles W, S, Up Arrow, Down Arrow
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        // 2. Create a direction vector (where to move)
+        Vector3 direction = new Vector3(moveX, moveY, 0);
+
+        // 3. Apply the movement
+        // We multiply by Time.deltaTime so it moves at the same speed on all computers
+        _playerTransform.position += direction * _speed * Time.deltaTime;
+
+        // Check if the Space bar was pressed this frame
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_numSeedsLeft > 0)
+            {
+                PlantSeed();
+            }
+            else
+            {
+                Debug.Log("No more seeds");
+            }
+        }
     }
 
+    [ContextMenu("Plant Seeds Now")]
     public void PlantSeed ()
     {
-        
+        GameObject plant = Instantiate(_plantPrefab, transform.position, Quaternion.identity) as GameObject;
+        _numSeedsLeft--;
+        _numSeedsPlanted++;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+
+        Debug.Log("You have planted seeds");
     }
 }
